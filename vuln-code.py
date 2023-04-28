00fb97737b921e28b46140d6af3096ef48962505
Adam Schoeman <adam@closehem.com>
2023-04-28 14:11:58 +0100

    Chaged scheduling time

--- vuln-code.py
+++ vuln-code.py
@@ -7,3 +7,16 @@ import commands
 template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
 
 hashed_password = hashlib.md5(request.params['foo']).hexdigest()
+
+sh.semgrep("--config", "https://semgrep.dev/p/r2c-CI")
+
+confurl = os.environ.get("SEMGREP_CONFIG_URL", "")
+# ruleid: string-concat
+sh.semgrep("--config {}".format(confurl))
+
+# ruleid: string-concat
+sh.semgrep(f"--config {confurl}")
+
+# ok: string-concat
+args = ["--config", confurl]
+sh.semgrep(*args)
