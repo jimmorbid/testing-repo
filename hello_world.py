9ec36dd50df6cd971f3bdfef23d73327a4d22986
Adam Schoeman <adam@AMWM-P14QG6Q9G7.local>
2023-04-28 12:57:41 +0100

    Changed the code to accept user input and output ROT13 cipher text

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,26 @@
 def hello_world():
     print("Hello, World!")
 
-if __name__ == "__main__":
-    hello_world()
+def rot13(text):
+    """
+    Convert text to ROT13 ciphered text.
+    """
+    result = ''
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                result += chr((ord(char) - 97 + 13) % 26 + 97)
+            else:
+                result += chr((ord(char) - 65 + 13) % 26 + 65)
+        else:
+            result += char
+    return result
+
+# Ask the user to enter some text
+text = input("Enter some text: ")
+
+# Convert the text to ROT13 ciphered text
+rot13_text = rot13(text)
+
+# Print the ROT13 ciphered text
+print("ROT13 ciphered text: " + rot13_text)
